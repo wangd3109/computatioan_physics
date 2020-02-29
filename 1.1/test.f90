568d598ebb819d23c4a3a05b79ec419b0b8ebf86
Duo Wang <wangd3109@gmail.com>
2020-02-29 10:07:33 +0100

    numerical differentiation, 3-point formula

--- 1.1/test.f90
+++ 1.1/test.f90
@@ -1,3 +1,18 @@
+! from 1.1 Numerical differentiation
+!! 3-point fomula
 program main
       implicit none
+      real :: h,x,f_prime,exact
+      integer :: steps
+
+      h=0.1
+      x=1.
+      exact=cos(x)
+      f_prime=(sin(x+h)-sin(x-h))/(2*h)
+
+      print*,exact,f_prime
+
+end program
+
+      
 
