f8c3fd8b30bd3d8a9f26b37a5bf53ea6bb5b46db
Duo Wang <wangd3109@gmail.com>
2020-02-29 11:57:09 +0100

    trapezoidal rule and simpson's rule

---
+++ 1.2/trapezoidal.f90
@@ -0,0 +1,31 @@
+!! 1.2 numerical quadrature
+! trapezoidal rule
+
+program main
+      implicit none
+      real :: h,x,integration,exact,x1,x2
+      integer :: steps,i
+
+      print*, 'Enter value of h (.le. 0 to stop)'
+      read*, h
+
+      x1=0.
+      x2=1.
+      steps=(x2-x1)/(2*h)
+      exact=exp(1.)-1.
+
+      do i=1,steps
+      x=x1+h
+      integration=integration+(h/2)*(exp(x-h)+2*exp(x)+exp(x+h))
+      x1=x1+2*h
+      end do
+
+      print *, exact,integration
+
+end program main
+
+!subroutine f(x)
+!        implicit none
+!        real :: x
+!
+!        f
