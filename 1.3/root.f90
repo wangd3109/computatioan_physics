fb2d948851307e63c1b1e52e242c55131f4a8580
Duo Wang <wangd3109@gmail.com>
2020-02-29 14:03:42 +0100

    finding roots

---
+++ 1.3/root.f90
@@ -0,0 +1,35 @@
+!! 1.3 finding roots
+
+program main
+      implicit none
+      real :: x,tolerance,diff,dx,f_old
+      integer :: iter
+
+      x=0
+      dx=20
+      iter=0
+      tolerance=1.e-06
+
+      f_old=sqrt(x)-5
+      
+      10 continue
+      iter=iter+1
+      x=x+dx
+      print *, iter,x,f_old,sqrt(x)-5
+      if (f_old*((sqrt(x))-5) .le. 0) then
+              x=x-dx
+              dx=dx/2
+      else
+              f_old=sqrt(x)-5
+      end if
+
+      if (abs(dx) .gt. tolerance) goto 10
+
+
+end program main      
+
+!function f(x)
+!        implicit none
+!        real :: x
+!        f=sqrt(x)-5
+!end function
