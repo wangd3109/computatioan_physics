822b1d829a805c20a5e9d42be39543d83b355922
Duo Wang <wangd3109@gmail.com>
2020-03-01 12:46:14 +0100

    finding root, the newton-raphson and secant method

---
+++ 1.3/secant_method.f90
@@ -0,0 +1,40 @@
+!! 1.3 finding roots
+! Secant method
+
+program main
+      implicit none
+      real :: x_0,x_1,x_2,x,tolerance,diff,f_0,f,f_prime
+      integer :: iter
+
+      x_0=1
+      x_1=2
+      iter=0
+      tolerance=1.e-06
+
+      10 continue
+      iter=iter+1
+      x_2=x_1-f(x_1)*(x_1-x_0)/(f(x_1)-f(x_0))
+      print *, 'iteration:',iter,'x_0:',x_0, 'x_1:',x_1, f(x_0), f(x_1)
+      diff=x_2-x_1
+      x_0=x_1
+      x_1=x_2
+      if ( abs(diff) .gt. tolerance ) goto 10
+
+
+end program main      
+
+function f_prime(x)
+        implicit none
+        real :: h,x,f_prime,f
+
+        h=0.001
+        f_prime=1/(12*h)*(f(x-2*h)-8*f(x-h)+8*f(x+h)-f(x+2*h))
+        
+end function
+
+function f(x)
+        implicit none
+        real :: f,x
+
+        f=x**2-5
+end function
