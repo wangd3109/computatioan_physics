86995fa29edc412a0bc5d3493dd24614bdb55a96
Duo Wang <wangd3109@gmail.com>
2020-03-15 10:57:09 +0100

    backup

--- project_1/p1.f90
+++ project_1/p1.f90
@@ -6,7 +6,7 @@ program main
       real :: term1, term2, h, t1, t2, theta
       integer :: i,j,k, steps
       
-      b0 = 0.1
+      b0 = 1
       db = 0.1
       rmax = 10
       V = 5
@@ -15,49 +15,51 @@ program main
 
       b = b0
       rmin = sqrt((e*b**2)/(e-v))
-      print *,rmin
+      print *,'rmin:',rmin
 
 !
-      do i = 0,100,1
+      do i = 0,0,1
       b=b0+db*i
- 
-      call analytical(b,rmax,v,e)
-      end do
-!      call integration(b,rmax)
-!
-      h = 0.001
-      b=0.3
 
+!! the analytical solution 
+!      call analytical(b,rmax,v,e,pi)
 
-      !! for the first term
+!! the numerical solution
+      h = 0.0001
+
+      !! THE FIRST TERM
       steps = (rmax-b)/(2*h)
       term1 = 0
       r=b
-!      print *, b,rmax,steps
-      do j = 1, steps
+      print *, b,rmax,steps
+      do j = 1, steps         ! do integration for the first term
+      print *, r, b
+      print *, 'mark',(1/r**2)*(1-(b/r)**2)**(1/2.),(1-(b/r)**2.)
+      print *, t1(r,b), t1(r+h,b), t1(r+2*h,b)
       term1=term1+(h/3)*(t1(r,b)+4*t1(r+h,b)+t1(r+2*h,b))
       r = r+2*h
       end do
 
-!      print *, 'term1:',term1
+ !     print *, "b",b,'rmax:',rmax,'term1:',term1
 
-      !! for the secon term
-      steps = (rmax-rmin)/(2*h)
-      term2 = 0
-      r=rmin
-!      print *, rmin, rmax, steps
-      do k = 1, steps
-!      print *, k, term2, t2(r,b,v,e)
-      term2 = (h/3)*(t2(r,b,V,E)+4*t2(r+h,b,V,E)+t2(r+2*h,b,V,E))
+      !! THE SECOND TERM
+
+!      print *,  t2(2.,b,V,E)
+!      steps = (rmax-rmin)/(2*h)
+!      term2 = 0
+!      r=rmin
+!      do k = 1, steps
+!      print *, 'r:',r,'b:',b,'v:',v,'e:',e
+!      print *, t2(r,b,v,e)
 !      term2 = term2+(h/3)*(t2(r,b,V,E)+4*t2(r+h,b,V,E)+t2(r+2*h,b,V,E))
-      r = r+2*h
-      end do
-!      print *, b,V,E,'term2:', term2
+!      r = r+2*h
+!      end do
+!      print *, 'rmin:',rmin,'rmax:',rmax,'term2:', term2
 
-      theta = 2*b*(term1-term2)
+!      theta = 2*b*(term1-term2)
 !      print *, 'numerical  solution:', b, theta
 
-!      end do
+      end do
 
      
 end program main
@@ -66,14 +68,15 @@ function t1(r,b)
         implicit none
         real :: r,b,t1
 
-        t1 = (1/r**2)*sqrt(1-(b/r)**2)
+        t1 = (1/r**2)*(1-(b/r)**2)**(-1/2.)
 end function
 
 function t2(r,b,V,E)
         implicit none
-        real :: r,b,V,E,t2
+        real :: r,b,V,E
+        real :: t2
 
-        t2 = (1/r**2)*sqrt(1-(b/r)**2-V/E) 
+        t2 = (1/r**2)*(1-(b/r)**2-V/E)**(-1/2.) 
 end function
 
 
@@ -110,21 +113,21 @@ end function f
 
 
 !!! to get the analytical solution
-subroutine analytical(b, rmax, V, E)
+subroutine analytical(b, rmax, V, E, pi)
         implicit none
-        real :: b, rmax, V, E 
+        real :: b, rmax, V, E ,pi
         real :: theta_1, theta_2
 
         if ( e < v) then
-                print *, "analytical solution1:",b, theta_1(b,rmax)
+                print *, "analytical solution1:",b, theta_1(b,rmax,pi)
 !                print *, v, e, b, rmax
         else
-                print *, "analytical solution2:",b, theta_2(b,rmax,V,E)
-                print *, v, e, b, rmax
+                print *, "analytical solution2:",b, theta_2(b,rmax,V,E,pi)
+!                print *, b, rmax
         end if
 end subroutine analytical
  
-function theta_1(b,rmax)
+function theta_1(b,rmax,pi)
         implicit none
         real :: b, rmax, pi
         real :: theta_1
@@ -133,12 +136,16 @@ function theta_1(b,rmax)
 
 end function
 
-function theta_2(b,rmax,V,E)
+function theta_2(b,rmax,V,E,pi)
         implicit none
         real :: b, rmax, V, E, pi
         real :: theta_2
 
-        theta_2 = 2*(asin(b/(rmax*sqrt(1-V/E)))) - asin(b/rmax)
+        theta_2 = 2*( asin(b/(rmax*sqrt(1-v/e))) -asin(b/rmax) )
+!        theta_2 = asin(1.)/(2*pi)*360
+!        theta_2 = pi
+!        theta_2 = asin(2/(10*sqrt(0.5)))*2/pi*360
+!        theta_2 = 2+b+rmax+v+e
 
 end function
 
