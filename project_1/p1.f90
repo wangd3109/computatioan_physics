1dabe114719956acf39df10dd10c3a4cc048cb41
Duo Wang <wangd3109@gmail.com>
2020-03-17 19:37:43 +0100

    works good

--- project_1/p1.f90
+++ project_1/p1.f90
@@ -6,7 +6,7 @@ program main
       real :: term1, term2, h, t1, t2, theta
       integer :: i,j,k, steps
       
-      b0 = 2
+      b0 = 1
       db = 0.1
       rmax = 10
       V = 5
@@ -14,12 +14,14 @@ program main
       pi = 4*atan(1.0)
 
       b = b0
-      rmin = sqrt((e*b**2)/(e-v))
-      print *,'rmin:',rmin
-
 !
-      do i = 0,0,1
+      do i = 0,100,1
       b=b0+db*i
+      theta = 0
+      rmin = sqrt((e*b**2)/(e-v))
+!      print *,'rmin:',rmin
+
+
 
 !! the analytical solution 
       call analytical(b,rmax,v,e,pi)
@@ -31,17 +33,11 @@ program main
       steps = (rmax-b)/(2*h)
       term1 = 0
       r=b+h
-      print *, b,rmax,steps
       do j = 1, steps         ! do integration for the first term
-      ! checking print
-!      print *, r, b
-!      print *, 'mark',(1/r**2)*(1-(b/r)**2)**(1/2.),(1-(b/r)**2.)
-!      print *, t1(r,b), t1(r+h,b), t1(r+2*h,b)
       term1=term1+(h/3)*(t1(r,b)+4*t1(r+h,b)+t1(r+2*h,b))
       r = r+2*h
       end do
-
-      print *, "b",b,'rmax:',rmax,'term1:',term1
+!      print *, "b",b,'rmax:',rmax,'term1:',term1
 
       !! THE SECOND TERM
 
@@ -50,12 +46,10 @@ program main
       term2 = 0
       r=rmin+h
       do k = 1, steps
-!      print *, 'r:',r,'b:',b,'v:',v,'e:',e
-!      print *, t2(r,b,v,e)
       term2 = term2+(h/3)*(t2(r,b,V,E)+4*t2(r+h,b,V,E)+t2(r+2*h,b,V,E))
       r = r+2*h
       end do
-      print *, 'rmin:',rmin,'rmax:',rmax,'term2:', term2
+!      print *, b,'rmin:',rmin,'rmax:',rmax,'term2:', term2
 
       theta = 2*b*(term1-term2)
       print *, 'numerical  solution:', b, theta
@@ -77,30 +71,9 @@ function t2(r,b,V,E)
         real :: r,b,V,E
         real :: t2
 
-        t2 = (1/r**2)*(1-(b/r)**2-V/E)**(-1/2.) 
+        t2 = (1./r**2)*(1.-(b/r)**2.-V/E)**(-1/2.) 
 end function
 
-
-!subroutine integration(x1,x2)
-!        implicit none
-!        real :: b,rmax,h,x1,x2,inte
-!        integer :: steps, i
-
-!        h = 0.001
-!        x1 = b
-!        x2 = rmax
-!        steps = (x2-x1)/(2*h)
-!        inte = 0
-        
-!        print *, "x1:",x1,"x2:",x2,'steps', steps
-!        do i = 1,steps
-!        print *, "iteration:",i, inte
-!        inte =inte+(h/3)*((1/x1**2)*sqrt(1-(b/x1)**2)+4*(1/(x1+h)**2)*sqrt(1-(b/(x1+h))**2)+(1/(x1+2*h)**2)*sqrt(1-(b/(x1+2*h))**2))
-!        x1 = x1+2*h
-!        end do
-
-!        print *, "term1:",inte
-!end subroutine
 
 function f(x,b,v,e)
         implicit none
@@ -121,10 +94,8 @@ subroutine analytical(b, rmax, V, E, pi)
 
         if ( e < v) then
                 print *, "analytical solution1:",b, theta_1(b,rmax,pi)
-!                print *, v, e, b, rmax
         else
                 print *, "analytical solution2:",b, theta_2(b,rmax,V,E,pi)
-!                print *, b, rmax
         end if
 end subroutine analytical
  
@@ -143,10 +114,6 @@ function theta_2(b,rmax,V,E,pi)
         real :: theta_2
 
         theta_2 = 2*( asin(b/(rmax*sqrt(1-v/e))) -asin(b/rmax) )
-!        theta_2 = asin(1.)/(2*pi)*360
-!        theta_2 = pi
-!        theta_2 = asin(2/(10*sqrt(0.5)))*2/pi*360
-!        theta_2 = 2+b+rmax+v+e
 
 end function
 
